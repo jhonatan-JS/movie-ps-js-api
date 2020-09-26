b421a907861921def09670732a8b982e1128bce2
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-26 08:47:08 -0400

    config heroku

--- src/controller/ProfileController.js
+++ src/controller/ProfileController.js
@@ -12,19 +12,15 @@ module.exports = {
   },
 
   async create(request, response) {
-    const nome = request.body;
+    const { nome } = request.body;
     const account_id = request.headers.authorization;
 
-    console.log(request.body);
+    try {
+      await connection('profiles').insert({nome, account_id});
 
-    const profiles = await connection('profiles')
-    .where('account_id', account_id)
-    .select('account_id')
-    ;
-
-    profiles === account_id
-      ? await connection('profiles').insert({nome, account_id})
-      : "Erro ao criar perfil"
+    } catch (error) {
+      "Erro ao criar perfil"
+    }
 
     return response.json(`Olá ${nome}`);
   },
